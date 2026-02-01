04ec7ce32d3780066dc0e8d7376ab8d37ce5fa04
frozenarchives <tirpudepranaykumar@gmail.com>
2026-02-01 00:10:01 +0000

    Deploying to gh-pages from @ frozenarchives/frozenarchives.github.io@5c60c6beb583062cc9e75ef7bc09d9a911f2ef24 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -9,7 +9,21 @@ ninja.data = [{
     handler: () => {
       window.location.href = "/";
     },
-  },{id: "nav-blog",
+  },{id: "nav-proposal",
+          title: "Proposal",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/proposal/";
+          },
+        },{id: "nav-proposal",
+          title: "Proposal",
+          description: "",
+          section: "Navigation",
+          handler: () => {
+            window.location.href = "/proposal/";
+          },
+        },{id: "nav-blog",
           title: "blog",
           description: "",
           section: "Navigation",
